c3f9720fcb7c3a7c4c22cc122b87100e7824844a
Danny Cardoso <dcardoso066@gmail.com>
2021-11-16 14:20:48 +0100

    MLFLOW params

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -9,9 +9,13 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
 
 class Trainer():
-    def __init__(self, X, y):
+    def __init__(self, X, y, experiment_name):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -19,6 +23,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -52,6 +57,28 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         return rmse
+    
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -68,7 +95,7 @@ if __name__ == "__main__":
     print('X_train and y_train defined...')
     
     # train and build  pipeline
-    tr = Trainer(X_train, y_train)
+    tr = Trainer(X_train, y_train, "[FR][Paris][Danny C batch 722] TaciFareModel")
     tr.set_pipeline()
     tr.run()
     print('Train data done...')
@@ -77,3 +104,8 @@ if __name__ == "__main__":
     print('rmse calculated...')
     print(rmse)
     print('End')
+    
+    tr.mlflow_log_metric("rmse", rmse)
+    tr.mlflow_log_param("model", "linear_model")
+    
+    print("MLFlow Done")
